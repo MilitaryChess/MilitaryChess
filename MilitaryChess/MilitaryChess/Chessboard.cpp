bcb9ef5fefe1be487ded1821d3beeba470998a3b
Zoey Bay <358319765@qq.com>
2020-04-08 20:41:44 +0800

    增加注释

--- MilitaryChess/MilitaryChess/Chessboard.cpp
+++ MilitaryChess/MilitaryChess/Chessboard.cpp
@@ -1,1 +1,13 @@
+/**
+* @file			Chessboard.cpp
+* @brief        Chessboard类的实现文件
+* @details		内容包括Chessboard类中的各种函数
+* @author       Bay
+* @date			2020.04.08
+* @version		0.0.1
+* @par Copyright (c):
+*       MilitaryChess
+* @par History:
+*   0.0.1: Bay, 2020.04.08, 创建文档\n
+*/
 #include "Chessboard.h"
--- MilitaryChess/MilitaryChess/Chessboard.h
+++ MilitaryChess/MilitaryChess/Chessboard.h
@@ -1,5 +1,32 @@
+/**
+* @file			Chessboard.h
+* @brief        Chessboard类的声明头文件
+* @details		Chessboard类主要是存储着棋盘中的信息的类
+* @author       Bay
+* @date			2020.04.08
+* @version		0.0.1
+* @par Copyright (c):
+*       MilitaryChess
+* @par History:
+*   0.0.1: Bay, 2020.04.08, 创建文档\n
+*/
 #pragma once
 class Chessboard
 {
+	/**
+	 * 构造函数
+	 * Chessboard类的构造函数
+	 * @param[in]   inArgName input argument description.
+	 * @param[out]  outArgName output argument description.
+	 * @retval  OK  成功
+	 * @retval  ERROR   错误
+	 * @par 标识符
+	 *      保留
+	 * @par 其它
+	 *      无
+	 * @par History:
+	 *      Bay 2020.04.08 创建\n
+	 */
+	Chessboard();
 };
 
