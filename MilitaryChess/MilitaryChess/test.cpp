4792d68768feaa432e3f7a8a0cc61f7a23d9d093
LonelyForLose <46125169+LonelyForLose@users.noreply.github.com>
2020-04-10 17:50:46 +0800

    添加测试，修改棋格类

--- MilitaryChess/MilitaryChess/test.cpp
+++ MilitaryChess/MilitaryChess/test.cpp
@@ -10,9 +10,12 @@
 * @par History:
 *   0.0.1: Bay, 2020.04.10, ´´½¨ÎÄµµ\n
 */
+#include <utility>
+#include <array>
 #include "iostream"
 #include "Chessboard.h"
 using namespace std;
+
 void test_Chess()
 {
 	char name = 'y';
@@ -21,11 +24,34 @@ void test_Chess()
 	cout << "movability: " << chess.movablity << endl;
 	cout << "type: " << chess.type << endl;
 	cout << "side: " << chess.side << endl;
+}
 
+void test_Check()
+{
+	cout << "start testing class Check: " << endl;
+	array<array<Check, 5>, 13> chessboard;
+	for (int r = 0; r < 13; r++) {
+		for (int c = 0; c < 5; c++) {
+			chessboard[r][c].assign(make_pair('m' - r, c));
+		}
+	}
+
+	for (int r = 0; r < 13; r++) {
+		for (int c = 0; c < 5; c++)
+			chessboard[r][c].output1();
+		cout << endl;
+		for (int c = 0; c < 5; c++)
+			chessboard[r][c].output2();
+		cout << endl; 
+		for (int c = 0; c < 5; c++)
+			chessboard[r][c].output3();
+		cout << endl;
+	}
 }
 
 int test_main()
 {
-	test_Chess();
+	//test_Chess();
+	test_Check();
 	return 0;
 }
